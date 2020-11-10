26ea339d77442e5aa6fdd9981d82feead3ae9fed
hopgoldy <hpgless@outlook.com>
2020-11-10 20:42:15 +0800

    translate: auth-tokens

--- src/pages/account/index.ts
+++ src/pages/account/index.ts
@@ -2,5 +2,6 @@ import entryPage from './entryPage'
 import badge from './badge'
 import email from './email'
 import password from './password'
+import tokens from './tokens'
 
-export default [entryPage, badge, email, password]
+export default [entryPage, badge, email, password, tokens]
