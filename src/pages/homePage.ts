4a2ef9b92fa704da241e44120b5c8a33e20307d2
LOSKIw <48372803+LOSKIw@users.noreply.github.com>
2020-11-25 22:59:04 +0800

    fix homepage无法追踪

--- src/pages/homePage.ts
+++ src/pages/homePage.ts
@@ -6,7 +6,10 @@ const content: PageContent = {
     hashs: [''],
 
     content: [
-        { 'en-US': 'MMO sandbox game for programmers', 'zh-CN': '为程序员量身打造的 MMO 沙盒游戏' }
+        { 'en-US': 'Status', 'zh-CN': '服务器状态', 'reuse': true },
+        { 'en-US': 'Status', 'zh-CN': '服务器状态', 'reuse': true },
+        { 'en-US': 'MMO sandbox game for programmers', 'zh-CN': '为程序员量身打造的 MMO 沙盒游戏' },
+        { 'en-US': 'It means "scripting creeps"', 'zh-CN': '含义为 “为creep编写脚本”', 'reuse': true }
     ]
 }
 
